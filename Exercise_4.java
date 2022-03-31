ae82cf69f844ec0307630deb34d4e7b4048ee52e
addepallisreelakshmi1294 <102629547+addepallisreelakshmi1294@users.noreply.github.com>
2022-03-31 18:24:52 -0400

    Update Exercise_4.java

--- Exercise_4.java
+++ Exercise_4.java
@@ -6,6 +6,53 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+	    
+	// Find sizes of two subarrays
+        int num1 = m - l + 1;
+        int num2 = r - m;
+	    
+	// Create temp arrays
+        int Left[] = new int[num1];
+        int Right[] = new int[num2];
+	    
+	// Copy data to temp arrays
+        for (int i = 0; i < num1; ++i)
+            Left[i] = arr[l + i];
+        for (int j = 0; j < num2; ++j)
+            Right[j] = arr[m + 1 + j];
+	    
+	// Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+  
+        // Initial index of merged subarray array
+        int k = l;
+	    
+        while (i < num1 && j < num2) {
+            if (Left[i] <= Right[j]) {
+                arr[k] = Left[i];
+                i++;
+            }
+            else {
+                arr[k] = Right[j];
+                j++;
+            }
+            k++;
+        }
+  
+        // Copy remaining elements of Left
+        while (i < n1) {
+            arr[k] = Left[i];
+            i++;
+            k++;
+        }
+  
+        // Copy remaining elements of Right
+        while (j < n2) {
+            arr[k] = Right[j];
+            j++;
+            k++;
+        }
+    }
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +61,17 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+	    
+	 if (l < r) {
+            int medium =l+ (r-l)/2;
+  
+            // Sort first and second halves
+            sort(arr, l, medium);
+            sort(arr, medium + 1, r);
+  
+            // Merge the sorted halves
+            merge(arr, l, medium, r);
+        }
     } 
   
     /* A utility function to print array of size n */
